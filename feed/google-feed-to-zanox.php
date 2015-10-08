c9d69277de861ad83b8c5458ba96b69d01f5a296
Tadeu Granemann <tadeugr@users.noreply.github.com>
2015-10-08 16:51:18 -0300

    Update google-feed-to-zanox.php

--- feed/google-feed-to-zanox.php
+++ feed/google-feed-to-zanox.php
@@ -9,7 +9,7 @@ foreach($json_google_feed["channel"]["item"] as $prod){
 	echo "<produto>";
         echo "<PROD_NUMBER>".trim($prod["g-mpn"])."</PROD_NUMBER>";
         echo "<PROD_NAME><![CDATA[".trim($prod["title"])."]]></PROD_NAME>";
-        echo "<PROD_URL><![CDATA[".trim($prod["link"])."?utm_source=zanetnox&utm_medium=xml&utm_campaign=geral]]></PROD_URL>";
+        echo "<PROD_URL><![CDATA[".trim($prod["link"])."?utm_source=zanox&utm_medium=xml&utm_campaign=geral]]></PROD_URL>";
         echo "<FOTO_URL><![CDATA[".trim($prod["g-image_link"])."]]></FOTO_URL>";
         echo "<IMG_MEDIUM><![CDATA[".trim($prod["g-image_link"])."]]></IMG_MEDIUM>";
         echo "<IMG_LARGE><![CDATA[".trim($prod["g-image_link"])."]]></IMG_LARGE>";
