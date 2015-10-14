617d404dc5f9fcfe7d283d77cda7ce7ed6f5e3a0
Tadeu Granemann <tadeugr@users.noreply.github.com>
2015-10-14 15:38:55 -0300

    Update cancel-order.php

--- order/cancel-order.php
+++ order/cancel-order.php
@@ -3,7 +3,7 @@
 require_once("PATH_TO_MAGENTO/app/Mage.php");
 Mage::app();
 
-$order_ids = array('100004545', '100004544');
+$order_ids = array('100004544', '100004545', '100004546');
 
 foreach($order_ids as $order_id){
     $order = Mage::getSingleton('sales/order');
