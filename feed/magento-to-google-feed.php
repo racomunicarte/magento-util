efb5a173a13e0c691a4658b3d9cf04cc01d4638f
Tadeu Granemann <tadeugr@users.noreply.github.com>
2015-10-20 10:15:27 -0200

    Update magento-to-google-feed.php

--- feed/magento-to-google-feed.php
+++ feed/magento-to-google-feed.php
@@ -17,39 +17,39 @@ if($products){
 	$installments = Mage::getStoreConfig('ipgintegracaodiretapagamento/cielo');
 	$interest_free = $installments["parcelas_sem_juros"];
 	
-	$xml .= "<?xml version=\"1.0\" encoding=\"UTF-8\"?>";
-	$xml .= "<rss xmlns:g=\"http://base.google.com/ns/1.0\" version=\"2.0\">";
-		$xml .= "<channel>";
-			$xml .= "<title>Store name</title>";
-			$xml .= "<link>Store URL</link>";
-			$xml .= "<description>Store Descripion</description>";
+	$xml .= "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n";
+	$xml .= "<rss xmlns:g=\"http://base.google.com/ns/1.0\" version=\"2.0\">\n";
+		$xml .= "<channel>\n";
+			$xml .= "\t<title>STORE NAME</title>\n";
+			$xml .= "\t<link>STORE URL</link>\n";
+			$xml .= "\t<description>STORE DESCRIPTION</description>\n";
 
 			foreach($products as $product){				
 				$loaded_product = Mage::getModel('catalog/product')->load($product->getId());
 				
-				$xml .= "<item>";
-					$xml .= "<g:id>".$loaded_product->getId()."</g:id>";
-					$xml .= "<title>".$loaded_product->getName()."</title>";
-					$xml .= "<description>".htmlspecialchars($loaded_product->getDescription())."</description>";
-					$xml .= "<link>".$loaded_product->getProductUrl()."</link>";
-					$xml .= "<g:image_link>".Mage::getModel('catalog/product_media_config')->getMediaUrl($loaded_product->getImage())."</g:image_link>";
-					$xml .= "<g:price>".number_format($loaded_product->getPrice(), 2, ".", "")." BRL</g:price>";
-					$xml .= "<g:sale_price>".number_format($loaded_product->getSpecialPrice(), 2, ".", "")." BRL</g:sale_price>";					
-					$xml .= "<g:installment><g:months>".$interest_free."</g:months><g:amount>".number_format($loaded_product->getSpecialPrice()/$interest_free, 2, ".", "")." BRL</g:amount></g:installment>";
+				$xml .= "\t<item>\n";
+					$xml .= "\t\t<g:id>".$loaded_product->getId()."</g:id>\n";
+					$xml .= "\t\t<title>".$loaded_product->getName()."</title>\n";
+					$xml .= "\t\t<description>".htmlspecialchars($loaded_product->getDescription())."</description>\n";
+					$xml .= "\t\t<link>".$loaded_product->getProductUrl()."</link>\n";
+					$xml .= "\t\t<g:image_link>".Mage::getModel('catalog/product_media_config')->getMediaUrl($loaded_product->getImage())."</g:image_link>\n";
+					$xml .= "\t\t<g:price>".number_format($loaded_product->getPrice(), 2, ".", "")." BRL</g:price>\n";
+					$xml .= "\t\t<g:sale_price>".number_format($loaded_product->getSpecialPrice(), 2, ".", "")." BRL</g:sale_price>\n";
+					$xml .= "\t\t<g:installment><g:months>".$interest_free."</g:months><g:amount>".number_format($loaded_product->getSpecialPrice()/$interest_free, 2, ".", "")." BRL</g:amount></g:installment>\n";
 
 					$stock = "out of stock";
 					if((float)Mage::getModel('cataloginventory/stock_item')->loadByProduct($loaded_product)->getQty() > 0){
 						$stock = "in stock";
 					}
-					$xml .= "<g:availability>".$stock."</g:availability>";
+					$xml .= "\t\t<g:availability>".$stock."</g:availability>\n";
 					
-					$xml .= "<g:shipping_weight>".$loaded_product->getWeight()." kg</g:shipping_weight>";
+					$xml .= "\t\t<g:shipping_weight>".$loaded_product->getWeight()." kg</g:shipping_weight>\n";
 					
 					$exp = date('Y-m-d',strtotime(date() . "+30 days"));
-					$xml .= "<g:expiration_date>".$exp."</g:expiration_date>";
+					$xml .= "\t\t<g:expiration_date>".$exp."</g:expiration_date>\n";
 					
-					$xml .= "<g:brand>Brand name</g:brand>";
-					$xml .= "<g:condition>new</g:condition>";
+					$xml .= "\t\t<g:brand>STORE BRAND</g:brand>\n";
+					$xml .= "\t\t<g:condition>new</g:condition>\n";
 
 					
 					$cats = $product->getCategoryIds();
@@ -57,16 +57,21 @@ if($products){
 					if(array_search(48, $cats) !== false){
 						$category = "Móveis > Cadeiras > Cadeiras de cozinha e sala de jantar";
 					} elseif(array_search(71, $cats) !== false){
-						$category = "Móveis > Cadeiras > Cadeiras de cozinha e sala de jantar";
-					}
+						$category = "Móveis > Mesas > Mesas de cozinha e sala de jantar";
+					} elseif(array_search(79, $cats) !== false){
+                                                $category = "Móveis > Conjuntos de móveis > Conjuntos de móveis para cozinha e sala de jantar";
+                                        } elseif(array_search(57, $cats) !== false){
+                                                $category = "Móveis > Móveis para escritório > Conjuntos de móveis para escritório";
+                                        }
 
-					$xml .= "<g:product_type>".$category."</g:product_type>";
-					$xml .= "<g:google_product_category>".$category."</g:google_product_category>";
+
+					$xml .= "\t\t<g:product_type>".$category."</g:product_type>\n";
+					$xml .= "\t\t<g:google_product_category>".$category."</g:google_product_category>\n";
 					
-					$xml .= "<g:mpn>".$loaded_product->getSku()."</g:mpn>";
-				$xml .= "</item>";
+					$xml .= "\t\t<g:mpn>".$loaded_product->getSku()."</g:mpn>\n";
+				$xml .= "\t</item>\n";
 			}
-		$xml .= "</channel>";
-	$xml .= "</rss>";
+		$xml .= "</channel>\n";
+	$xml .= "</rss>\n";
 }
 echo $xml;
