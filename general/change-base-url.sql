cf2753137e9c947867da427de1ac9f2d29b0f18a
Tadeu Granemann <tadeugr@users.noreply.github.com>
2017-05-05 18:51:19 -0500

    Create change-base-url.sql

---
+++ general/change-base-url.sql
@@ -0,0 +1,2 @@
+update core_config_data set value = 'http://domainname/' where path = 'web/unsecure/base_url';
+update core_config_data set value = 'http://domainname/' where path = 'web/secure/base_url';
