558f94963912013d0352dde81e10bac859f67eae
Tadeu Granemann <tadeugr@users.noreply.github.com>
2017-05-16 15:06:51 -0500

    Update truncate-tables.sql

--- general/truncate-tables.sql
+++ general/truncate-tables.sql
@@ -1,5 +1,8 @@
+--SET foreign_key_checks = 0;
+
 TRUNCATE dataflow_batch_export ; 
 TRUNCATE dataflow_batch_import ; 
+
 TRUNCATE log_customer ; 
 TRUNCATE log_quote ; 
 TRUNCATE log_summary ; 
@@ -9,4 +12,14 @@ TRUNCATE log_url_info ;
 TRUNCATE log_visitor ; 
 TRUNCATE log_visitor_info ; 
 TRUNCATE log_visitor_online ; 
+
 TRUNCATE report_event ;
+TRUNCATE report_compared_product_index;
+TRUNCATE report_viewed_product_aggregated_daily;
+TRUNCATE report_viewed_product_aggregated_monthly;
+TRUNCATE report_viewed_product_aggregated_yearly;
+TRUNCATE report_viewed_product_index;
+
+TRUNCATE sales_bestsellers_aggregated_daily;
+TRUNCATE sales_bestsellers_aggregated_monthly;
+TRUNCATE sales_bestsellers_aggregated_yearly;
